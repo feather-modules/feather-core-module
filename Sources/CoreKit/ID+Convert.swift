f0183a0fe430093645340e83f2332482c9adf0ae
Tibor Bodecs <mail.tib@gmail.com>
2024-03-02 16:35:24 +0100

    add core kit

---
+++ Sources/CoreKit/ID+Convert.swift
@@ -0,0 +1,31 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tibor Bodecs on 02/03/2024.
+//
+
+import CoreInterfaceKit
+import DatabaseQueryKit
+
+extension ID {
+
+    init(_ key: Key<T>) {
+        self.init(key.rawValue)
+    }
+
+    func toKey() -> Key<T> {
+        .init(rawValue)
+    }
+}
+
+extension Key {
+
+    init(_ id: ID<T>) {
+        self.init(id.rawValue)
+    }
+
+    func toID() -> ID<T> {
+        .init(rawValue)
+    }
+}
