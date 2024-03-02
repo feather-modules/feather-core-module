21d98a9b1864455f8cefa29d1e176bf700ac29ba
Tibor Bodecs <mail.tib@gmail.com>
2024-03-02 16:25:46 +0100

    add second convertable func

--- Sources/CoreInterfaceKit/Encodable+Convert.swift
+++ Sources/CoreInterfaceKit/Encodable+Convert.swift
@@ -16,4 +16,12 @@ extension Encodable {
     ) throws -> T {
         try decoder.decode(T.self, from: try encoder.encode(self))
     }
+    
+    public func convert<T: Decodable>(
+        encoder: JSONEncoder = .init(),
+        decoder: JSONDecoder = .init()
+    ) throws -> T {
+        try convert(to: T.self, encoder: encoder, decoder: decoder)
+    }
 }
+
--- Tests/CoreInterfaceKitTests/CoreInterfaceKitTests.swift
+++ Tests/CoreInterfaceKitTests/CoreInterfaceKitTests.swift
@@ -6,12 +6,41 @@
 //
 
 import XCTest
-
 @testable import CoreInterfaceKit
 
 final class CoreInterfaceKitTests: XCTestCase {
 
     func testExample() async throws {
         XCTAssertTrue(true)
+    }
+    
+    func testConvertable1() async throws {
+        struct Foo: Codable {
+            let key: String
+        }
+        
+        struct Bar: Codable {
+            let key: String
+        }
+        
+        let key = "key"
+        let foo = Foo(key: key)
+        let bar = try foo.convert(to: Bar.self)
+        XCTAssertEqual(bar.key, key)
+    }
+    
+    func testConvertable2() async throws {
+        struct Foo: Codable {
+            let key: String
+        }
+        
+        struct Bar: Codable {
+            let key: String
+        }
+        
+        let key = "key"
+        let foo = Foo(key: key)
+        let bar: Bar = try foo.convert()
+        XCTAssertEqual(bar.key, key)
     }
 }
