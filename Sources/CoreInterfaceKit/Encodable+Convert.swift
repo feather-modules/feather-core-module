8a54347d95c4cbf137561d09f8129b37280a06e3
Tibor Bodecs <mail.tib@gmail.com>
2024-03-04 10:30:02 +0100

    add conversion error

--- Sources/CoreInterfaceKit/Encodable+Convert.swift
+++ Sources/CoreInterfaceKit/Encodable+Convert.swift
@@ -7,6 +7,28 @@
 
 import Foundation
 
+public enum ConversionError: Error {
+    case notPossible(String)
+    case unknown(Error)
+}
+
+extension CodingKey {
+    /// returns a coding key as a path key string.
+    fileprivate var pathKeyValue: String {
+        if let value = intValue {
+            return String(value)
+        }
+        return stringValue
+    }
+}
+
+extension Array where Element == CodingKey {
+    /// returns a path key using a dot character as a separator.
+    fileprivate var pathKeyValue: String {
+        map(\.pathKeyValue).joined(separator: ".")
+    }
+}
+
 extension Encodable {
 
     public func convert<T: Decodable>(
@@ -14,7 +36,32 @@ extension Encodable {
         encoder: JSONEncoder = .init(),
         decoder: JSONDecoder = .init()
     ) throws -> T {
-        try decoder.decode(T.self, from: try encoder.encode(self))
+        do {
+            return try decoder.decode(T.self, from: try encoder.encode(self))
+        }
+        catch DecodingError.dataCorrupted(let context) {
+            let message = context.debugDescription
+            throw ConversionError.notPossible(message)
+        }
+        catch DecodingError.keyNotFound(let key, _) {
+            let path = key.pathKeyValue
+            let message = "Coding key `\(path)` not found."
+            throw ConversionError.notPossible(message)
+        }
+        catch DecodingError.valueNotFound(_, let context) {
+            let path = context.codingPath.pathKeyValue
+            let message = "Value not found for `\(path)` key."
+            throw ConversionError.notPossible(message)
+        }
+        catch DecodingError.typeMismatch(let type, let context) {
+            let path = context.codingPath.pathKeyValue
+            let message =
+                "Type mismatch for `\(path)` key, expected `\(type)` type."
+            throw ConversionError.notPossible(message)
+        }
+        catch {
+            throw ConversionError.unknown(error)
+        }
     }
 
     public func convert<T: Decodable>(
--- Tests/CoreInterfaceKitTests/CoreInterfaceKitTests.swift
+++ Tests/CoreInterfaceKitTests/CoreInterfaceKitTests.swift
@@ -44,4 +44,35 @@ final class CoreInterfaceKitTests: XCTestCase {
         let bar: Bar = try foo.convert()
         XCTAssertEqual(bar.key, key)
     }
+
+    func testConvertable3() async throws {
+        struct Foo: Codable {
+            let key: String
+        }
+
+        struct Bar: Codable {
+            let bar: String
+        }
+
+        let key = "key"
+        let foo = Foo(key: key)
+        do {
+            let bar: Bar = try foo.convert()
+            XCTFail("Conversion should fail.")
+        }
+        catch let error as ConversionError {
+            switch error {
+            case .notPossible(let message):
+                // ok
+                let expectation = "Coding key `bar` not found."
+                XCTAssertEqual(message, expectation)
+            case .unknown(_):
+                XCTFail("Conversion should fail.")
+            }
+        }
+        catch {
+            XCTFail("\(error.localizedDescription)")
+        }
+
+    }
 }
