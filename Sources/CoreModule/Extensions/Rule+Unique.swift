02f3dd523f56444270398b79ac799b7b2f43d0a9
Tibor Bödecs <mail.tib@gmail.com>
2024-03-13 14:14:54 +0100

    Add id rule validation (#10)
    - extend rule with id validation

---
+++ Sources/CoreModule/Extensions/Rule+Unique.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//
+//
+//  Created by Tibor Bodecs on 13/03/2024.
+//
+
+import CoreModuleInterface
+import DatabaseQueryKit
+import FeatherValidation
+
+extension Rule where T: RawRepresentable & Object {
+
+    public static func unique<QB: QueryBuilderCount>(
+        queryBuilder: QB,
+        fieldKey: QB.Row.FieldKeys,
+        message: String? = nil,
+        originalKey: T? = nil
+    ) -> Self {
+        .init(
+            message: message ?? "The value should be unique."
+        ) { value in
+            let count = try await queryBuilder.count(
+                filter: .init(
+                    field: fieldKey,
+                    operator: .equal,
+                    value: value
+                )
+            )
+            if let originalKey {
+                if originalKey == value {
+                    guard count == 1 else {
+                        throw RuleError.invalid
+                    }
+                }
+                else {
+                    guard count == 0 else {
+                        throw RuleError.invalid
+                    }
+                }
+            }
+            else {
+                guard count == 0 else {
+                    throw RuleError.invalid
+                }
+            }
+        }
+    }
+}
