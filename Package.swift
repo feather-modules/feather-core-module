7db807aae73d9e6599d15679d7ea6e146130a99a
Tibor Bodecs <mail.tib@gmail.com>
2024-03-13 14:06:11 +0100

    add rule id validation

--- Package.swift
+++ Package.swift
@@ -20,6 +20,8 @@ let package = Package(
         .package(url: "https://github.com/feather-framework/feather-relational-database", .upToNextMinor(from: "0.2.0")),
         .package(url: "https://github.com/feather-framework/feather-database-kit", .upToNextMinor(from: "0.4.0")),
         .package(url: "https://github.com/feather-framework/feather-openapi-kit", .upToNextMinor(from: "0.8.0")),
+        .package(url: "https://github.com/feather-framework/feather-access-control", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/feather-framework/feather-validation", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         .target(name: "CoreModuleInterface"),
@@ -28,6 +30,8 @@ let package = Package(
             dependencies: [
                 .product(name: "NanoID", package: "swift-nanoid"),
                 .product(name: "FeatherRelationalDatabase", package: "feather-relational-database"),
+                .product(name: "FeatherACL", package: "feather-access-control"),
+                .product(name: "FeatherValidation", package: "feather-validation"),
                 .product(name: "DatabaseQueryKit", package: "feather-database-kit"),
                 .target(name: "CoreModuleInterface"),
             ]
