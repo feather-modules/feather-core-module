fcf062eda829f28beb7d356673baea6944e0cfc7
Tibor Bödecs <mail.tib@gmail.com>
2024-03-15 19:16:46 +0100

    CI updates (#11)
    - test updated ci workflows
    - add equatable & encodable unique validation rule

--- Sources/CoreModule/Extensions/Rule+Unique.swift
+++ Sources/CoreModule/Extensions/Rule+Unique.swift
@@ -9,6 +9,45 @@ import CoreModuleInterface
 import DatabaseQueryKit
 import FeatherValidation
 
+extension Rule where T: Equatable & Encodable {
+
+    public static func unique<QB: QueryBuilderCount>(
+        queryBuilder: QB,
+        fieldKey: QB.Row.FieldKeys,
+        message: String? = nil,
+        originalEmail: T? = nil
+    ) -> Self {
+        .init(
+            message: message ?? "The value should be unique."
+        ) { value in
+            let count = try await queryBuilder.count(
+                filter: .init(
+                    field: fieldKey,
+                    operator: .equal,
+                    value: value
+                )
+            )
+            if let originalEmail {
+                if originalEmail == value {
+                    guard count == 1 else {
+                        throw RuleError.invalid
+                    }
+                }
+                else {
+                    guard count == 0 else {
+                        throw RuleError.invalid
+                    }
+                }
+            }
+            else {
+                guard count == 0 else {
+                    throw RuleError.invalid
+                }
+            }
+        }
+    }
+}
+
 extension Rule where T: RawRepresentable & Object {
 
     public static func unique<QB: QueryBuilderCount>(
