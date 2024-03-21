4f7a7f633e22f1bc0fdbc16acd43d004f4246019
Tibor Bodecs <mail.tib@gmail.com>
2024-03-21 12:17:02 +0100

    update deps

--- Package.swift
+++ Package.swift
@@ -16,26 +16,19 @@ let package = Package(
         .library(name: "CoreOpenAPIGeneratorKit", targets: ["CoreOpenAPIGeneratorKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/binarybirds/swift-nanoid", from: "1.0.0"),
         .package(url: "https://github.com/feather-framework/feather-relational-database", .upToNextMinor(from: "0.2.0")),
-        .package(url: "https://github.com/feather-framework/feather-database-kit", .upToNextMinor(from: "0.6.0")),
+        .package(url: "https://github.com/feather-framework/feather-database-kit", .upToNextMinor(from: "0.7.0")),
         .package(url: "https://github.com/feather-framework/feather-openapi-kit", .upToNextMinor(from: "0.8.0")),
-        .package(url: "https://github.com/feather-framework/feather-access-control", .upToNextMinor(from: "0.2.0")),
         .package(url: "https://github.com/feather-framework/feather-validation", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         .target(
-            name: "CoreModuleKit",
-            dependencies: [
-                .product(name: "FeatherACL", package: "feather-access-control"),
-            ]
+            name: "CoreModuleKit"
         ),
         .target(
             name: "CoreModule",
             dependencies: [
-                .product(name: "NanoID", package: "swift-nanoid"),
                 .product(name: "FeatherRelationalDatabase", package: "feather-relational-database"),
-                .product(name: "FeatherACL", package: "feather-access-control"),
                 .product(name: "FeatherValidation", package: "feather-validation"),
                 .product(name: "DatabaseQueryKit", package: "feather-database-kit"),
                 .target(name: "CoreModuleKit"),
--- Tests/CoreModuleTests/CoreModuleTests.swift
+++ Tests/CoreModuleTests/CoreModuleTests.swift
@@ -6,56 +6,9 @@
 //
 
 import CoreModuleKit
-import FeatherACL
 import XCTest
 
-@testable import CoreModule
-
-public struct Foo: ModuleInterface {
-
-    public enum ACL: ACLSet {
-
-        static public var all: [Permission] =
-            []
-            + Bar.ACL.all
-    }
-
-}
-
-extension Permission {
-
-    static func foo(_ context: String, action: Action) -> Self {
-        .init(namespace: "system", context: context, action: action)
-    }
-}
-
-extension Permission {
-
-    static func fooBar(action: Action) -> Self {
-        .foo("bar", action: action)
-    }
-}
-
-public struct Bar: RepositoryInterface {
-
-    public struct ACL: ACLSet {
-
-        static let list: Permission = .fooBar(action: .list)
-        static let create: Permission = .fooBar(action: .create)
-
-        static public var all: [Permission] = [
-            ACL.list
-        ]
-    }
-}
-
 final class CoreModuleTests: XCTestCase {
-
-    func testACL() async throws {
-        for i in Foo.ACL.all {
-            print(i.key)
-        }
-    }
 
     func testConvertable1() async throws {
         struct Foo: Codable {
